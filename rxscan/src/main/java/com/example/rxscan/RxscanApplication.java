6ce45a6cf6de2ef41e586d6c844088bfdd117cfb
Syed Farhan Hyder Abidi <syed.farhanhyderabidi@maybank.com>
2024-01-31 21:41:48 +0530

    RXSCAN-01 #comment Moving project folder from local to remote

---
+++ rxscan/src/main/java/com/example/rxscan/RxscanApplication.java
@@ -0,0 +1,13 @@
+package com.example.rxscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxscanApplication.class, args);
+	}
+
+}
---
+++ rxscan/src/test/java/com/example/rxscan/RxscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rxscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
